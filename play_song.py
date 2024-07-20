d3e6663884e2b0911fe3456265a59de0cca71ae1
Postigic <143212308+Postigic@users.noreply.github.com>
2024-07-20 13:36:10 +0800

    Update play_song.py

--- play_song.py
+++ play_song.py
@@ -1,57 +1,53 @@
 import pygame
 import time
-import librosa
 import syllapy
 from song_transcript import *
 from utils import style
 
 
-def count_syllables(sentence):
-    words = sentence.split()
-    total_syllables = sum(syllapy.count(word) for word in words)
-    return total_syllables
+def calculate_print_speed(text, duration):
+    total_syllables = sum(syllapy.count(word) for word in text.split())
+    if total_syllables <= 1:
+        return 0.03
+
+    return duration / (total_syllables * 10)
 
 
-def slow_print(text: str, speed):
+def slow_print(text, speed, colour):
     for character in text:
-        print(character, end="", flush=True)
+        print(colour + character + style.RESET, end="", flush=True)
         time.sleep(speed)
 
 
-def print_lyrics(audio_file_path, styles):
-    audio_data, sample_rate = librosa.load(audio_file_path)
-    tempo, beat_times = librosa.beat.beat_track(y=audio_data, sr=sample_rate)
-    beat_times = librosa.frames_to_time(beat_times, sr=sample_rate)
-
+def print_lyrics(audio_file, styles):
     lyrics_to_print = []
     for index, lyric in enumerate(transcript):
-        syllable_count = count_syllables(lyric["text"])
-        timing = lyric["duration"] / syllable_count / 10
+        print_speed = calculate_print_speed(lyric["text"], lyric["duration"])
         lyrics_to_print.append(
-            (lyric["start"], lyric["text"], styles[index], timing))
+            (lyric["start"], lyric["text"], styles[index], print_speed))
 
     pygame.mixer.init()
-    pygame.mixer.music.load(audio_file_path)
+    pygame.mixer.music.load(audio_file)
+    total_duration = (pygame.mixer.Sound(audio_file)).get_length()
     pygame.mixer.music.play()
 
     current_lyric_index = 0
     running = True
 
     while running:
-        if current_lyric_index < len(transcript):
-            current_lyric = transcript[current_lyric_index]
-            start_time = current_lyric["start"]
-            current_time = pygame.mixer.music.get_pos() / 1000
-            if current_time >= start_time:
-                start_time, text, current_style, timing = lyrics_to_print[current_lyric_index]
-                if len(text) <= 5:
-                    print(current_style + f"\n{text}" +
-                          style.RESET, end="", flush=True)
-                else:
-                    slow_print(current_style +
-                               f"\n{text}" + style.RESET, timing)
-                current_lyric_index += 1
-            handle_visuals(current_time)
+        current_lyric = transcript[current_lyric_index]
+        start_time = current_lyric["start"]
+        current_time = pygame.mixer.music.get_pos() / 1000
+
+        if current_time >= start_time:
+            start_time, text, current_style, print_speed = lyrics_to_print[current_lyric_index]
+            slow_print(f"\n{text}", print_speed, current_style)
+            current_lyric_index += 1
+        handle_visuals(current_time)
+
+        if current_lyric_index >= len(transcript):
+            time.sleep((total_duration + 1) - current_time)
+            running = False
 
     pygame.quit()
 
@@ -67,10 +63,10 @@ def handle_visuals(current_time):
 
 
 if __name__ == '__main__':
-    audio_file_path = r"D:\vs code projects\world.execute(me);\world-execute-me.mp3"
+    audio_file = r"D:\vs code projects\world.execute(me);\world-execute-me.mp3"
     styles = [style.RED] * len(transcript)
 
-    for index, color in indices_styles.items():
-        styles[index] = color
+    for index, colour in indices_styles.items():
+        styles[index] = colour
 
-    print_lyrics(audio_file_path, styles)
+    print_lyrics(audio_file, styles)
